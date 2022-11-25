a1616c695cc5a8435ece10f977517e6b228619b0
Perdita Edge-Partington <perdita.edge.partington@gmail.com>
2022-11-25 11:27:21 +0000

    fix bundler version

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,6 @@
 class BookmarksController < ApplicationController
-  before_action :set_list, only: %i[new create]
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
 
   def new
     @list = List.find(params[:list_id])
@@ -10,7 +11,12 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     @bookmark.save
-    redirect_to list_path(@list)
+    redirect_to root_path(@list)
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to root_path(@bookmark.list), status: :see_other
   end
 
   private
@@ -20,7 +26,11 @@ class BookmarksController < ApplicationController
   end
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
   end
 
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -16,7 +16,7 @@ class ListsController < ApplicationController
     @list = List.new(list_params)
     @list.save
 
-    redirect_to list_path(@list)
+    redirect_to root_path(@list)
   end
 
   private
